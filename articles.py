0f98e945f7db0437016f1f2861aaed5291c262be
Dannemiller <55891360+Dannemiller@users.noreply.github.com>
2022-02-01 11:47:51 -0600

    Add files via upload
    Time out errors no longer crash program, however timed out articles are lost which means potential answers are lost. Additional changes made to make testing easier, and to lessen the memory impact of article objects.

--- articles.py
+++ articles.py
@@ -7,105 +7,74 @@ import asyncio
 
 def create_wiki_link(partial):
     return 'https://en.wikipedia.org/wiki/' + partial
-# (client.get(url) for url in hyperlink_list)
-def generator_experiment(hyperlink_list):
-    for count, url in hyperlink_list:
-        print(count)
-
-class Article:
-    def __init__(self, url):
-        self.article_url = url # Direct provided URL to article
-        self.distance_to_target = float('inf') # Current shortest distance to target, positive inf if target not found
-        self.next_article = None # Points to the next article in shortest sequence
-        self.hyperlink_list = self.create_hyperlink_list() # List of article hyperlinks found in main article
-        self.partial = self.create_partial_url(url) # Create partial tag of current article
-        self.article_name = self.find_title() # Retrieves title of current article
 
 
-    def create_partial_url(self, url):
-        # Initial searching found %#\"?&;=/ symbols to indicators of non article pages, update as findings change
-        # Symbols might not be useful as direct URLs should either be manually selected or previously filtered
-        # TODO: Review need for symbol filtering as project progresses
-        search_url = re.compile(r"wikipedia.org/wiki/[^%#\"?&;=/]+")
-        partial = re.findall(search_url, url)[0][19:]
-        return partial
-
-    def create_partial_hyperlink(self, hyperlink):
-        # Banned list filters common non article pages that make it through regex filtering, list is probably non
-        # exhaustive and leaves rom for false positive bans
-        # TODO: Review banned list and/or come up with more accurate alternatives
-        banned_list = "Category, Wikipedia, Template, Template_talk, Help, commons, Talk"
-        banned_list = banned_list.split(', ')
-        # Initial searching found %#\"?&;=/ symbols to indicators of non article pages, update as findings change
-        # Regex strips partial tags of banned pages down to a recognizable form
-        # combine with banned list and only article links should pass all checks
-        # Regex search sometimes removes needed URL information so final partial tag does not use the filter
-        search = re.compile(r"a href=\"/wiki/[^%#\"?&;=/:]+")
-        link_match = re.findall(search, str(hyperlink))
-        length = len(link_match)
-        if length == 1:
-            link_match = list(link_match)[0]
-            # [14:] strips link_match down to just the partial tag that can be used in creating links
-            link_match = list(link_match)[14:]
-            final = ''
-            for j in link_match:
-                final += j
-            if final in banned_list:
-                return
-            holder = ''
-            for letter in link_match:
-                holder += letter
-            return holder
-
-    def make_soup(self):
-        page = requests.get(self.article_url)
-        return BeautifulSoup(page.content, "html.parser")
-
-    def find_title(self):
-        soup = self.make_soup()
-        results = soup.find(id='firstHeading')
-        return results.text.strip()
-
-    def create_hyperlink_list(self):
-        soup = self.make_soup()
-        body = soup.find(id='bodyContent')
-        hyperlinks = body.find_all('a', href=True, class_=False)
-        hyperlink_list = []
-        for i in hyperlinks:
-            holder = self.create_partial_hyperlink(i)
-            if holder is None:
-                continue
-            hyperlink_list.append(holder)
-        return hyperlink_list
-
-class Article_Async:
-    def __init__(self, url, page=None):
-        self.article_url = url  # Direct provided URL to article
-        if page is None:
-            self.page = requests.get(self.article_url)
+class ArticleAsync:
+    def __init__(self, page):
+        self.page = page
         self.soup = self.make_soup()
-        self.distance_to_target = float('inf')  # Current shortest distance to target, positive inf if target not found
         self.next_article = None  # Points to the next article in shortest sequence
+        self.distance_to_target = float('inf')
         self.article_list = []  # List of article hyperlinks found in main article, start blank to avoid infinite recursion
-        self.partial = self.create_partial_url(url)  # Create partial tag of current article
         self.article_name = self.find_title()  # Retrieves title of current article
+        self.hyperlink_set = set()
+        self.create_hyperlink_set()
+        del self.soup
+        del self.page
 
+    def __gt__(self, other):
+        if type(other) is ArticleAsync:
+            return self.article_name > other.article_name
+        elif type(other) is str:
+            return self.article_name > other
+        else:
+            raise TypeError
 
+    def __ge__(self, other):
+        if type(other) is ArticleAsync:
+            return self.article_name >= other.article_name
+        elif type(other) is str:
+            return self.article_name >= other
+        else:
+            raise TypeError
 
-    def create_partial_url(self, url):
-        # Initial searching found %#\"?&;=/ symbols to indicators of non article pages, update as findings change
-        # Symbols might not be useful as direct URLs should either be manually selected or previously filtered
-        # TODO: Review need for symbol filtering as project progresses
-        search_url = re.compile(r"wikipedia.org/wiki/[^%#\"?&;=/]+")
-        partial = re.findall(search_url, url)[0][19:]
-        return partial
+    def __lt__(self, other):
+        if type(other) is ArticleAsync:
+            return self.article_name < other.article_name
+        elif type(other) is str:
+            return self.article_name < other
+        else:
+            raise TypeError
+
+    def __le__(self, other):
+        if type(other) is ArticleAsync:
+            return self.article_name <= other.article_name
+        elif type(other) is str:
+            return self.article_name <= other
+        else:
+            raise TypeError
+
+    def __eq__(self, other):
+        if type(other) is ArticleAsync:
+            return self.article_name == other.article_name
+        elif type(other) is str:
+            return self.article_name == other
+        else:
+            raise TypeError
+
+    def __ne__(self, other):
+        if type(other) is ArticleAsync:
+            return self.article_name != other.article_name
+        elif type(other) is str:
+            return self.article_name != other
+        else:
+            raise TypeError
 
     def create_partial_hyperlink(self, hyperlink):
         # Banned list filters common non article pages that make it through regex filtering, list is probably non
         # exhaustive and leaves rom for false positive bans
         # TODO: Review banned list and/or come up with more accurate alternatives
-        banned_list = "Category, Wikipedia, Template, Template_talk, Help, commons, Talk"
-        banned_list = banned_list.split(', ')
+        banned_list = {'Category', 'Wikipedia', 'Template', 'Template_talk', 'Help', 'commons', 'Talk'}
         # Initial searching found %#\"?&;=/ symbols to indicators of non article pages, update as findings change
         # Regex strips partial tags of banned pages down to a recognizable form
         # combine with banned list and only article links should pass all checks
@@ -128,42 +97,17 @@ class Article_Async:
             return holder
 
     def make_soup(self):
-        return BeautifulSoup(self.page.content, "html.parser")
+        soup = BeautifulSoup(self.page.content, "html.parser")
+        return soup
 
     def find_title(self):
         results = self.soup.find(id='firstHeading')
         return results.text.strip()
 
-    async def gather_pages(self, hyperlink_list):
-        async with httpx.AsyncClient() as client:
-            tasks = (client.get(url) for url in hyperlink_list)
-            return await asyncio.gather(*tasks)
-
-    def create_article_list(self):
+    def create_hyperlink_set(self):
         body = self.soup.find(id='bodyContent')
         hyperlinks = body.find_all('a', href=True, class_=False)
-        hyperlink_list = []
-        repeats = set()
         for i in hyperlinks:
             holder = self.create_partial_hyperlink(i)
-
-            if holder is None:
-                continue
-
-            if holder in repeats:
-                continue
-            else:
-                hyperlink_list.append(create_wiki_link(holder))
-                repeats.add(holder)
-
-        print(len(hyperlink_list))
-
-        reqs = asyncio.run(self.gather_pages(hyperlink_list))
-
-        print(len(reqs), len(hyperlink_list))
-        for v, j in enumerate(reqs):
-            soup_hold = BeautifulSoup(j.content, "html.parser")
-            result = soup_hold.find(id='firstHeading')
-            print(f'found title: {result.text.strip()} for link: {hyperlink_list[v]}\n')
-
-
+            if holder is not None:
+                self.hyperlink_set.add(create_wiki_link(holder))
--- search.py
+++ search.py
@@ -1,40 +1,61 @@
 import articles
+import httpx
+import asyncio
+import requests
+import sys
 
-search_basket = set() # Global set that contains all previously searched articles to avoid loops
-# and searching pages that have already not given any results
 
-def depth_search(start, target, max_depth, current_depth=0):
-    for i in start.hyperlink_list:
-        current_link = articles.create_wiki_link(i)
-        current_article = articles.Article(current_link) # Create article object for the next layer in the search
-        print(' '*current_depth, current_article.article_name)
-        if current_article.article_name == target.article_name: # If the current search object matches target return
-            print('Target located, returning to surface')
-            current_article.distance_to_target = current_depth
-            return current_article
-        elif current_depth >= max_depth: # If max depth is reached, return empty handed
-            print(' '*current_depth, f'Can not dive deeper, {current_article.partial}')
-            current_article.distance_to_target = float('inf')
+async def articles_batch(hyperlink_list):
+    async with httpx.AsyncClient(follow_redirects=True) as client:
+        tasks = (client.get(url) for url in hyperlink_list)
+        pages = await asyncio.gather(*tasks, return_exceptions=True)
+        return pages
+
+
+def create_article_list(current):
+    slice_size = 1000
+    size = len(current.hyperlink_set)
+    print(size)
+    hundred_holder = []
+    hyperlinks = list(current.hyperlink_set)
+    for item in range(size // slice_size):
+        hundred_holder.append(hyperlinks[:slice_size * (item + 1)])
+    hundred_holder.append(hyperlinks[-1 * (size % slice_size):])
+    for hundred_links in hundred_holder:
+        pages = asyncio.run(articles_batch(hundred_links))
+        for page in pages:
+            if not issubclass(type(page), Exception):
+                current.article_list.append(articles.ArticleAsync(page))
+
+
+def confirmation(current):
+    intermediate = articles.ArticleAsync(requests.get('https://en.wikipedia.org/wiki/Albert_Einstein'))
+    current.article_list.sort()
+    for spot, i in enumerate(current.article_list):
+        if intermediate.article_name == i.article_name:
+            print(f'Confirmation at spot: {spot}')
             return
-        # if current_article.article_name in search_basket: #
-        #    print('PING')
-        #    return
 
-        successor = depth_search(current_article, target, max_depth=max_depth, current_depth=current_depth+1) # Search continues, got to next deep link
-        # search_basket.add(current_article.article_name)
+def depth_search_async(current, target, max_depth, current_depth=0):
+    create_article_list(current)
+    confirmation(current)
+    for spot, item in enumerate(current.article_list):
+        found = None
+        next_depth = current_depth + 1
+        print(f'Current depth: {current_depth}, spot: {spot}')
+        if item.article_name == target.article_name:
+            print('Target found rising')
+            return target.article_name
+        elif current_depth <= max_depth:
+            print(f'Diving {item.article_name}')
+            found = depth_search_async(item, target, max_depth, next_depth)
+        else:
+            print('No target rising')
+            return
 
-        if successor is None: # Search came back empty handed, go to next partial in list
-            continue
-
-        print(f'Surfacing, current article {current_article.article_name}')
-        current_article.distance_to_target = current_depth
-        current_article.next_article = successor
-        return current_article
-    return
-
-def breadth_search(start, target):
-    for i in range(8):
-        check = depth_search(start, target, max_depth=i)
-        if check is not None:
-            return check
-    return
+        if found is not None:
+            if next_depth < current.distance_to_target:
+                current.distance_to_target = next_depth
+                current.next_article = found
+        # Program is memory intensive, need to free up as much as possible during runtime
+        current.article_list[spot] = None
